e5a7e75faa597386e8cadb551db1d6903e77589d
Mouson03 <147017948+Mouson03@users.noreply.github.com>
2024-06-09 13:45:33 +0800

    Update stock_monitor.py

--- stock_monitor.py
+++ stock_monitor.py
@@ -1,1 +1,15 @@
-print('hello action')
+import requests
+import json
+
+def send_dingtalk_message(message):
+    webhook_url = "https://oapi.dingtalk.com/robot/send?access_token=f9a9044d1c878dfd92d71a18704f623b4cd88296a1762dce858e372dcef76202"
+    headers = {'Content-Type': 'application/json'}
+    data = {
+        "msgtype": "text",
+        "text": {
+            "content": message
+        }
+    }
+    response = requests.post(webhook_url, headers=headers, data=json.dumps(data))
+
+send_dingtalk_message('测试actions')
