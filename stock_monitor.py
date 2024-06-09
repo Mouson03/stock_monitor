1fbcf30bc82a3500331f97637fc44bc1a7942523
Mouson03 <147017948+Mouson03@users.noreply.github.com>
2024-06-09 14:32:50 +0800

    Update stock_monitor.py

--- stock_monitor.py
+++ stock_monitor.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import akshare
 
 def send_dingtalk_message(message):
     webhook_url = "https://oapi.dingtalk.com/robot/send?access_token=f9a9044d1c878dfd92d71a18704f623b4cd88296a1762dce858e372dcef76202"
@@ -13,3 +14,5 @@ def send_dingtalk_message(message):
     response = requests.post(webhook_url, headers=headers, data=json.dumps(data))
 
 send_dingtalk_message('测试actions')
+data = ak.stock_zh_index_daily_em(symbol='sz399998', start_date='20240520', end_date='20240701')   
+print(data)
