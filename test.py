b08b308b84b4efa2ed836f1c99733c485c0818ee
Mouson03 <147017948+Mouson03@users.noreply.github.com>
2024-06-11 21:19:52 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,20 @@
-print('hello')
+import requests
+import json
+
+
+
+def send_dingtalk_message(message):
+    webhook_url = "https://oapi.dingtalk.com/robot/send?access_token=f9a9044d1c878dfd92d71a18704f623b4cd88296a1762dce858e372dcef76202"
+    headers = {'Content-Type': 'application/json'}
+    data = {
+        "msgtype": "text",
+        "text": {
+            "content": message
+        }
+    }
+    response = requests.post(webhook_url, headers=headers, data=json.dumps(data))
+
+
+
+send_dingtalk_message('actions每日监控未持有的程序开始运行')
+send_dingtalk_message("未持有部分出现买入信号:\n")
