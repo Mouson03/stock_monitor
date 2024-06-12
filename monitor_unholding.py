96a9b386cff41787c775998b02f675ead0a0da69
Mouson03 <147017948+Mouson03@users.noreply.github.com>
2024-06-12 15:49:52 +0800

    Update monitor_unholding.py

--- monitor_unholding.py
+++ monitor_unholding.py
@@ -23,16 +23,16 @@ def analysis():
             new_names = {'收盘': 'close', '最高': 'high', '最低': 'low','成交量':'volume'}
             data.rename(columns=new_names, inplace=True)
     
-            data['SD'] = data['close'].rolling(window=period).std(ddof=1)
-            data['MB'] = data['close'].rolling(window=period).mean()
-            data['UB'] = data['MB'] + k * data['SD']
-            data['LB'] = data['MB'] - k * data['SD']
-            data['low_boll_percentage'] = data['low'].sub(data['LB']).div(data['UB'].sub(data['LB']))
-            #data['high_boll_percentage'] = data['high'].sub(data['LB']).div(data['UB'].sub(data['LB']))
-            #if data['low_boll_percentage'].iloc[-1]>=0:
-            if data['low_boll_percentage'].iloc[-1]>=-1:
-            #if data['low_boll_percentage'].iloc[-1]>=0 and data['low_boll_percentage'].iloc[-2]<0:
-                buy_signal_index_code.append(index_code)
+        data['SD'] = data['close'].rolling(window=period).std(ddof=1)
+        data['MB'] = data['close'].rolling(window=period).mean()
+        data['UB'] = data['MB'] + k * data['SD']
+        data['LB'] = data['MB'] - k * data['SD']
+        data['low_boll_percentage'] = data['low'].sub(data['LB']).div(data['UB'].sub(data['LB']))
+        #data['high_boll_percentage'] = data['high'].sub(data['LB']).div(data['UB'].sub(data['LB']))
+        #if data['low_boll_percentage'].iloc[-1]>=0:
+        if data['low_boll_percentage'].iloc[-1]>=-1:
+        #if data['low_boll_percentage'].iloc[-1]>=0 and data['low_boll_percentage'].iloc[-2]<0:
+            buy_signal_index_code.append(index_code)
 
     return buy_signal_index_code
 
