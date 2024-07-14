55735c2996eddb6ee11334efe5c7f95f12e4030a
Mouson03 <147017948+Mouson03@users.noreply.github.com>
2024-07-14 09:44:33 +0800

    Update ETF_PremiumRate_Record_Alert.py

--- ETF_PremiumRate_Record_Alert.py
+++ ETF_PremiumRate_Record_Alert.py
@@ -1,24 +1,28 @@
+#1.溢价率和涨跌幅单位是%  2.记录每天中午休息时的溢价率(即actions每周1-5早11点15分运行)
 import akshare as ak
 import pandas as pd
 import datetime
-import os
+import requests
+import json
 
 # ETF列表，替换为你的基金代码列表
-etf_list = ['159941', '513100', '159632', '159513', '513300', '159501', '513390', '159696', '513110', '159659', '159660', '513870']
+etf_list = ['159941','513100','159632','159513','513300','159501','513390','159696','513110','159659','159660','513870']
 
 # 持有的ETF及其对应的溢价率阈值
 holdings = {
-    '159941': 1.0,
-    '513100': 2.0
+    #'000000':0.0    #添加这个避免无持有ETF时字典为空
+    '159941': 4.0,
+    '513100': 4.0,
+    '159501': 4.0
     # 添加更多的持有ETF和阈值
 }
 
 # 获取当前日期
 today = datetime.datetime.today().strftime('%Y-%m-%d')
 
-# 获取昨天纳指100涨跌幅
-data_NDX = ak.index_us_stock_sina(symbol=".NDX")
-change_percentage = ((data_NDX['close'].iloc[-1] - data_NDX['close'].iloc[-2]) / data_NDX['close'].iloc[-2] * 100).round(2)
+#获取昨天纳指100涨跌幅
+data_NDX= ak.index_us_stock_sina(symbol=".NDX")
+change_percentage=((data_NDX['close'].iloc[-1] - data_NDX['close'].iloc[-2]) / data_NDX['close'].iloc[-2] * 100).round(2)
 
 # 超出阈值的ETF列表
 alert_messages = []
@@ -27,7 +31,7 @@ alert_messages = []
 df = ak.fund_etf_spot_em()
 
 # 创建一个空的字典用于存储结果
-result_dict = {"日期": today, "昨晚纳指100涨跌幅": change_percentage}
+result_dict = {"日期": today,"昨晚纳指100涨跌幅":change_percentage}
 
 for etf in etf_list:
     # 获取当前ETF的行
@@ -42,7 +46,7 @@ for etf in etf_list:
         premium_rate = (latest_price - iopv) / iopv
 
         # 将溢价率添加到字典中
-        result_dict[f"{etf}"] = round(premium_rate * 100, 2)
+        result_dict[f"{etf}"] = round(premium_rate*100,2)
 
         # 检查持有的ETF的溢价率是否超过阈值
         if etf in holdings and result_dict[f"{etf}"] > holdings[etf]:
@@ -52,19 +56,36 @@ for etf in etf_list:
 # 转换字典为DataFrame
 result_df = pd.DataFrame([result_dict])
 
-# 文件路径
-csv_path = os.getenv("CSV_PATH", "artifacts/etf_premium_rates.csv")
+# 文件名
+file_name = "etf_premium_rates.csv"
 
-if os.path.exists(csv_path):
-    # 读取现有的CSV文件
-    existing_df = pd.read_csv(csv_path)
-    # 追加新数据
-    result_df = pd.concat([existing_df, result_df], ignore_index=True)
-else:
-    # 如果文件不存在，直接使用新数据
-    pass
+# 读取现有的CSV文件
+existing_df = pd.read_csv(file_name)
+
+# 追加新数据
+result_df = pd.concat([existing_df, result_df], ignore_index=True)
 
 # 保存结果到csv文件
-result_df.to_csv(csv_path, index=False)
+result_df.to_csv(file_name, index=False)
 
-print(f"{today}的ETF溢价率已记录，持有ETF溢价率阈值已检查。")
+# 发送超出阈值的ETF信息至钉钉
+def send_dingtalk_message(message):
+    webhook_url = "https://oapi.dingtalk.com/robot/send?access_token=f9a9044d1c878dfd92d71a18704f623b4cd88296a1762dce858e372dcef76202"
+    headers = {'Content-Type': 'application/json'}
+    data = {
+        "msgtype": "text",
+        "text": {
+            "content": message
+        }
+    }
+    response = requests.post(webhook_url, headers=headers, data=json.dumps(data))
+for message in alert_messages:
+    send_dingtalk_message(alert_messages)
+
+# 记录运行日志
+log_message = f"{today}  \"ETF溢价率记录及预警\"  程序运行完毕"
+log_file = "actions_running_log.txt"
+with open(log_file, 'a') as f:
+    f.write(f"{log_message}\n")
+
+print(f"{today}的ETF溢价率已记录，持有ETF溢价率阈值已检查,运行日志已记录。")
