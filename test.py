da2ed99cf457533dc5e78d0b2895d484b5dca627
Mouson03 <147017948+Mouson03@users.noreply.github.com>
2025-06-15 20:38:48 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
-# test.py
-
-# 打开文件test.txt，追加写入模式
-with open('test.txt', 'a') as file:
-    file.write('test\n')
+import akshare as ak
+#A股股票-新浪
+df = ak.stock_zh_a_daily(symbol='sz002807' , adjust="qfq")
+print(df)
